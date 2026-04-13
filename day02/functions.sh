383594409f584ca59120aa5d2101d8de49208080
Ubuntu <ubuntu@ip-172-31-34-202.us-east-2.compute.internal>
2026-04-13 13:05:26 +0000

    modified file

--- day02/functions.sh
+++ day02/functions.sh
@@ -7,19 +7,19 @@ disclaimer
 # This is function definition
 
 function is_loyal() {
-	read -p "Jetha ne mud ke kise dekha: " bandi
-	read -p "jetha ka pyaar %" pyaar
+	read -p "$1 ne mud ke kise dekha: " bandi
+	read -p "$1 ka pyaar %" pyaar
 
 if [[ $bandi == "daya bhabhi" ]];
 then
-	echo "jetha is loyal"
+	echo "$1 is loyal"
 elif [[ $pyaar -ge 100 ]];
 then
-	echo "jetha is loyal"
+	echo "$1 is loyal"
 else
-	echo "jetha is not loyal"
+	echo "$1 is not loyal"
 fi
 }
 
 # This is function call
-is_loyal
+is_loyal "tom"
